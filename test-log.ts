c3df9443086ed37c79a040ad743e548651be3228
0x8e8fb-p <prabhakaracharaya@gmail.com>
2026-04-17 01:56:13 +0530

    chore: remove throwaway root test and debug scripts

--- test-log.ts
+++
@@ -1,8 +0,0 @@
-import { logSearchAction, getPlatformStats } from "./src/app/actions/flightActions";
-
-async function run() {
-  await logSearchAction("DEL", "BOM", "2024-05-15", 5);
-  const stats = await getPlatformStats();
-  console.log("Stats:", stats);
-}
-run();
--- test-prisma-local.js
+++
@@ -1,3 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-console.log("Prisma datasource url is:", prisma._engineConfig?.env?.DATABASE_URL || "unknown");
